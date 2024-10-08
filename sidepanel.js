0966c5856c1983d132eb7c2a6e380ee7f4c53bfb
Aarosh Das <aaroshdas@gmail.com>
2024-10-08 00:12:30 -0400

    actual dict created + syn and ayn

--- sidepanel.js
+++ sidepanel.js
@@ -1,6 +1,34 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const API_KEY=  "AIzaSyCsGDpgFakD5RwMe0txpkDqT3do-aWfSMk";
+
+async function generateSynonyms (phrase){
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  let prompt = `Generate a defintion for: ${phrase}, in one sentence`;
+  let result = await model.generateContent(prompt);
+  document.getElementById("definitions").innerHTML = result.response.text().toString();
+
+  
+
+  prompt = `Generate 20 synonyms for the word: ${phrase}, in a comma separated list`;
+  result = await model.generateContent(prompt);
+  document.getElementById("synonyms").innerHTML = result.response.text().toString();
+
+  prompt = `Generate 7 antonyms for the word: ${phrase}, in a comma separated list`;
+  result = await model.generateContent(prompt);
+  document.getElementById("antonyms").innerHTML = result.response.text().toString();
+  
+
+}
+
+
 chrome.storage.session.get('lastPhrase', ({ lastPhrase }) => {
   document.getElementById("selectedPhrase").innerHTML = lastPhrase;
+  generateSynonyms(lastPhrase)
 })
+
 
 
 chrome.storage.session.onChanged.addListener((change) => {
@@ -11,4 +39,5 @@ chrome.storage.session.onChanged.addListener((change) => {
   }
 
   document.getElementById("selectedPhrase").innerHTML  = newWord.newValue;
+  generateSynonyms(newWord.newValue)
 });
