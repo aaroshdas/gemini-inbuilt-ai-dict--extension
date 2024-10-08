53417b0234ef2e686d05beb0924391a9d3f6231c
Aarosh Das <aaroshdas@gmail.com>
2024-10-08 10:38:11 -0400

    local storage settings, setting dropdown, json prompts

--- sidepanel.js
+++ sidepanel.js
@@ -1,40 +1,104 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenerativeAI,SchemaType } = require("@google/generative-ai");
 
 const API_KEY=  "AIzaSyCsGDpgFakD5RwMe0txpkDqT3do-aWfSMk";
 
-async function generateDefinitions (phrase, settings){
-  console.log(settings)
+
+chrome.storage.session.get('settings', ({ settings }) => {
+  if(!settings){
+    document.getElementById("showDef").checked = true;
+    document.getElementById("showSyn").value = 16;
+    document.getElementById("showAnt").value = 8;
+  }
+  else{
+    document.getElementById("showDef").checked = settings[0];
+    document.getElementById("showSyn").value = settings[1];
+    document.getElementById("showAnt").value = settings[2];
+  }
+  setSettings([document.getElementById("showDef").checked, document.getElementById("showSyn").value, document.getElementById("showAnt").value])
+})
+
+
+function setSettings(settings){
+  if(settings[1] > 16){settings[1] = 16;}
+  if(settings[1] < 0){settings[1] = 0;}
+
+  if(settings[2] > 8){settings[2] = 8;}
+  if(settings[2] < 0){settings[2] = 0;}
+  
+  chrome.storage.session.set({ settings: settings });
+}
+
+async function generateDefinitions (phrase){
   const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  
+  let setting = [true, 16, 8]
+  chrome.storage.session.get('settings', ({ settings }) => {
+    setting = [settings[0], settings[1], settings[2]]
+  });
+  const schema  = {
+    description: "List of results",
+    type: SchemaType.ARRAY,
+    items: {
+      type: SchemaType.OBJECT,
+      properties: {
+        item: {
+          type: SchemaType.STRING,
+          description: "words",
+          nullable: false,
+        },
+      },
+      required: ["item"],
+    },
+  }
+  const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash" , 
+    generationConfig: {
+      responseMimeType: "application/json",
+      responseSchema: schema,
+    }}
+  );
 
-
-  if(settings[0] == true){
-    let prompt = `Generate a defintion for: ${phrase}, in one sentence`;
+  console.log(setting)
+  if(setting[0] == true){
+    let prompt = `Generate a complex defintion for: ${phrase}, in one sentence`;
     let result = await model.generateContent(prompt);
-    document.getElementById("definitions").innerHTML = result.response.text().toString();
+    const obj = JSON.parse(result.response.text())
+    document.getElementById("definitions").innerHTML = obj[0]["item"];
   }
   else{document.getElementById("definitions").innerHTML = ""}
   
-  if(settings[1] == true){
-    let prompt2 = `Generate 20 synonyms for the word: ${phrase}, in a comma separated list`;
+  if(setting[1] > 0){
+    let prompt2 = `Generate ${setting[1]} synonyms for the word: ${phrase}, and for each synonym, write a short definition`;
     let result2 = await model.generateContent(prompt2);
-    document.getElementById("synonyms").innerHTML = result2.response.text().toString();
-  }
-  else{document.getElementById("synonyms").innerHTML =""}
+    const obj = JSON.parse(result2.response.text())
 
-  if(settings[2]== true){
-    let prompt3 = `Generate 7 antonyms for the word: ${phrase}, in a comma separated list`;
-    let result3 = await model.generateContent(prompt3);
-    document.getElementById("antonyms").innerHTML = result3.response.text().toString();
+    document.getElementById("synonymList").innerHTML = ""
+    for(let i =0; i < obj.length; i++){
+      document.getElementById("synonymList").append(Object.assign(document.createElement('li'), {textContent: obj[i]["item"]}))
+    }
   }
-  else{ document.getElementById("antonyms").innerHTML = ""}
+  else{document.getElementById("synonymList").innerHTML =""}
+
+  if(setting[2] > 0){
+    let prompt3 = `Generate ${setting[2]} antonyms for the word: ${phrase}, for each antonym, write a short definition`;
+    let result3 = await model.generateContent(prompt3);
+    
+    const obj = JSON.parse(result3.response.text())
+
+    document.getElementById("antonymList").innerHTML = ""
+    for(let i =0; i < obj.length; i++){
+      document.getElementById("antonymList").append(Object.assign(document.createElement('li'), {textContent: obj[i]["item"]}))
+    }
+
+  }
+  else{ document.getElementById("antonymList").innerHTML = ""}
 
 }
 
 
 chrome.storage.session.get('lastPhrase', ({ lastPhrase }) => {
   document.getElementById("selectedPhrase").innerHTML = lastPhrase;
-  generateDefinitions(lastPhrase, [document.getElementById("showDef").checked, document.getElementById("showSyn").checked, document.getElementById("showAnt").checked])
+  generateDefinitions(lastPhrase);
 })
 
 
@@ -47,13 +111,29 @@ chrome.storage.session.onChanged.addListener((change) => {
   }
 
   document.getElementById("selectedPhrase").innerHTML  = newWord.newValue;
-  generateDefinitions(newWord.newValue, [document.getElementById("showDef").checked, document.getElementById("showSyn").checked, document.getElementById("showAnt").checked])
+  generateDefinitions(newWord.newValue);
 });
 
 
 document.getElementById("submitSearch").addEventListener("click", ()=>{
   document.getElementById("selectedPhrase").innerHTML = document.getElementById("searchBar").value.toString();
-  generateDefinitions(document.getElementById("searchBar").value.toString(), [document.getElementById("showDef").checked, document.getElementById("showSyn").checked, document.getElementById("showAnt").checked]);
+  generateDefinitions(document.getElementById("searchBar").value.toString());
   document.getElementById("searchBar").value = ""
+})
 
+
+document.getElementById("submitSettings").addEventListener("click", ()=>{
+  setSettings([document.getElementById("showDef").checked, document.getElementById("showSyn").value, document.getElementById("showAnt").value])
+  if(document.getElementById("settingDropdown").classList.contains("dropdown-full")){
+    document.getElementById("settingDropdown").classList.remove("dropdown-full");
+  }
+})
+
+document.getElementById("openSettings").addEventListener("click", ()=>{
+  if(document.getElementById("settingDropdown").classList.contains("dropdown-full")){
+    document.getElementById("settingDropdown").classList.remove("dropdown-full");
+  }
+  else{
+    document.getElementById("settingDropdown").classList.add("dropdown-full");
+  }
 })
